5dd37de3afd9ad39fd74f55d34fbcf16212c4b60
ignis <ignis.siu6i@gmail.com>
2024-01-02 02:32:37 +0900

    fix: fix #2 and try to fix #1

--- uva-725/main.c
+++ uva-725/main.c
@@ -24,48 +24,75 @@ int hasSameNumber(int num) {
 	return 0;
 }
 
-int main(void)
-{
-	int input;
+int hasSameNumber2(int num, int num2) {
+	int nums[10];
 
-	do {
-		scanf("%d", &input);
-		int count = 0;
+	for (int i = 0; i < 5; ++i) {
+		nums[i] = num % 10;
+		num /= 10;
+	}
+	for (int i = 0; i < 5; ++i) {
+		nums[i + 5] = num2 % 10;
+		num2 /= 10;
+	}
 
-		int dividend = 1235;
-		int divisor = 1234;
-
-		while (dividend < 100000) {
-			if (hasSameNumber(dividend)) {
-				divisor = 1234;
-				++dividend;
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j < 10; ++j) {
+			if (i == j) {
 				continue;
 			}
-			while (divisor < dividend) {
-				if (hasSameNumber(divisor)) {
-					++divisor;
+			if (nums[i] == nums[j]) {
+				return 1;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int main(void)
+{
+	int N;
+
+	do {
+		scanf("%d", &N);
+		int count = 0;
+
+		int dividend_min = N * 1234;
+
+		for (int dividend = dividend_min; dividend < 100000; ++dividend) {
+			if (hasSameNumber(dividend)) {
+				continue;
+			}
+			int divisor_min = dividend / (N + 1);
+			if (divisor_min < 1234) {
+				divisor_min = 1234;
+			}
+			int divisor_max = dividend / (N - 1);
+			if (divisor_max > dividend) {
+				divisor_max = dividend;
+			}
+			for (int divisor = divisor_min + 1; divisor < divisor_max; ++divisor) {
+				if (hasSameNumber2(dividend, divisor)) {
 					continue;
 				}
 				int quotient = dividend / divisor;
 				int remainder = dividend - quotient * divisor;
-				if (remainder == 0 && quotient == input) {
+				if (remainder == 0 && quotient == N) {
 					++count;
-					printf("%d / %d = %d\n", dividend, divisor, input);
+					printf("%05d / %05d = %d\n", dividend, divisor, N);
 				}
 				//else {
 				//	printf("%d / %d = %d ... %d\n", dividend, divisor, quotient, remainder);
 				//}
-				++divisor;
 			}
-			divisor = 1234;
-			++dividend;
 		}
 
 		if (count == 0) {
-			printf("There are no solutions for %d.\n", input);
+			printf("There are no solutions for %d.\n", N);
 		}
 		printf("\n");
-	} while (input != 0);
+	} while (N != 0);
 
 	return 0;
 }
