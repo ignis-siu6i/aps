ad998f7a47e44cfa405c4661a83674b6e354f6b4
ignis <ignis.siu6i@gmail.com>
2024-01-28 03:26:56 +0900

    feat: solve ALDS1_10_A with DP <- fix issue #17

--- ALDS1_10_A/main.cpp
+++ ALDS1_10_A/main.cpp
@@ -2,17 +2,27 @@
 
 using namespace std;
 
+int memo[44];
+
 int fibonacci(int n) {
+	if (memo[n]) {
+		return memo[n];
+	}
 	if (n == 0 || n == 1) {
 		return 1;
 	}
-	return fibonacci(n - 1) + fibonacci(n - 2);
+	memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+	return memo[n];
 }
 
 int main(void)
 {
 	int n;
 	cin >> n;
+
+	for (register int i = 0; i < 44; ++i) {
+		memo[i] = 0;
+	}
 
 	cout << fibonacci(n) << endl;
 
